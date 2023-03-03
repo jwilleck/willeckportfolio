d46ce5b6dd64a2fbf10c9e4d4725dfa510cd7f43
jwilleck <113205608+jwilleck@users.noreply.github.com>
2023-03-03 15:57:15 -0600

    code clean up

--- app.js
+++ app.js
@@ -10,5 +10,10 @@ const observer = new IntersectionObserver((entries) => {
     });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+
+
+
+
